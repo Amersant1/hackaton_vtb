436bf229c06adb2e303703f2c49354b8be0f682b
sergeybudygin <svbudygin@yandex.ru>
2023-10-14 09:32:27 +0300

    av_tm update

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
 import requests
 import json
 
-json1 = {
-    "latitude": 55.4507,
-    "longitude": 37.3656,
-    "limit": 10,
-    "offset": 5,
-    # "usd_available": True,
-    # "euro_available": True,
-}
-x = requests.post(url="http://192.168.88.217:5000/api/get_points", json=json.dumps(json1))
+json1 = {"latitude": 55.4507, "longitude": 37.3656, "limit": 1, "offset": 5,"usd_available":True,
+"euro_available":True}
+x = requests.post(url="http://localhost:9002/api/update_number_of_people_info", json=json.dumps(json1))
 print(x.content)
